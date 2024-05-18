8918c29398262430e253a6a0c403c21b0a0f2ccc
Hmddev23 <hamid.maira709@gmail.com>
2024-05-18 12:44:53 +0000

    task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    
+    """
+    start_page = (page - 1) * page_size
+    end_page = page * page_size
+    return (start_page, end_page)
